8ced9869c2bcc5c5af84ef569d43a4ed1dc67ee3
moriroKim <moriroKim@gmail.com>
2024-08-07 13:44:55 +0900

    css => tailwind migration complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import "./App.css";
-import { contents } from "./assets/data/data";
-import Content from "./components/Content";
-import Header from "./components/Header";
+import './App.css';
+import { contents } from './assets/data/data';
+import Content from './components/Content';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
-  );
+    return (
+        <main className="bg-indigo-950">
+            <Header />
+            <section className="flex flex-wrap justify-center gap-5 px-5 py-5">
+                {contents.map((el) => (
+                    <Content key={el.id} content={el} />
+                ))}
+            </section>
+        </main>
+    );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export default function Header() {
-  return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
-      </ul>
-    </header>
-  );
+    return (
+        <header className="flex justify-between items-center py-[10px] px-5 bg-black">
+            <h2 className="text-white">OZ코딩스쿨</h2>
+            <ul className="flex items-center justify-center gap-5">
+                <li className="list-none font-normal text-white text-[13px]">로그인</li>
+                <li className="list-none font-normal text-white text-[13px]">회원가입</li>
+                <li className="list-none font-normal text-white text-[13px]">내클래스</li>
+            </ul>
+        </header>
+    );
 }
